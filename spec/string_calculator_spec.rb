2ccabd400f2f6741f3dcdcf46efad6da6c885887
Battula Pavan Kumar <pavan.kumar02@yahoo.com>
2025-04-14 19:31:23 +0530

    Fixed Rubocop errors and optimized the code

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 require 'spec_helper'
 require_relative '../string_calculator'
@@ -6,19 +7,19 @@ RSpec.describe StringCalculator do
   let(:calc) { StringCalculator.new }
 
   it 'returns 0 for an empty string' do
-    expect(calc.add("")).to eq(0)
+    expect(calc.add('')).to eq(0)
   end
 
   it 'returns number for a single number string' do
-    expect(calc.add("1")).to eq(1)
+    expect(calc.add('1')).to eq(1)
   end
 
   it 'returns the sum of two numbers' do
-    expect(calc.add("1,5")).to eq(6)
+    expect(calc.add('1,5')).to eq(6)
   end
 
   it 'returns the sum of multiple numbers' do
-    expect(calc.add("1,2,3,4")).to eq(10)
+    expect(calc.add('1,2,3,4')).to eq(10)
   end
 
   it 'handles new lines between numbers' do
@@ -30,10 +31,10 @@ RSpec.describe StringCalculator do
   end
 
   it 'raises an error if a negative number is included' do
-    expect { calc.add("1,-2,3") }.to raise_error("Negative numbers not allowed -2")
+    expect { calc.add('1,-2,3') }.to raise_error('Negative numbers not allowed -2')
   end
-  
+
   it 'raises an error listing all negative numbers' do
-    expect { calc.add("1,-2,-5,3") }.to raise_error("Negative numbers not allowed -2,-5")
+    expect { calc.add('1,-2,-5,3') }.to raise_error('Negative numbers not allowed -2,-5')
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,24 +1,35 @@
+# frozen_string_literal: true
+
+# StringCalculator performs addition on a string of numbers with support for
+# custom delimiters and error handling for negative numbers.
 class StringCalculator
   def add(numbers)
     return 0 if numbers.strip.empty?
 
-    delimiter = /,|\n/
+    delimiter, number_string = extract_delimiter_and_numbers(numbers)
+    nums = number_string.split(delimiter).map(&:to_i)
 
-    if numbers.start_with?("//")
-      delimiter_line, numbers = numbers.split("\n", 2)
-      custom_delimiter = delimiter_line[2..]
-      delimiter = Regexp.escape(custom_delimiter)
-      delimiter = /#{delimiter}/
-    end
-    
-    nums = numbers.split(delimiter).map(&:to_i)
-    negative_numbers = nums.select { |n| n < 0 }
-
-    unless negative_numbers.empty?
-      raise "Negative numbers not allowed #{negative_numbers.join(',')}"
-    end
+    check_for_negatives(nums)
 
     nums.sum
   end
+
+  private
+
+  def extract_delimiter_and_numbers(input)
+    if input.start_with?('//')
+      delimiter_line, numbers = input.split("\n", 2)
+      custom_delimiter = Regexp.escape(delimiter_line[2..])
+      [Regexp.new(custom_delimiter), numbers]
+    else
+      [/[,|\n]/, input]
+    end
+  end
+
+  def check_for_negatives(nums)
+    negative_numbers = nums.select(&:negative?)
+    return if negative_numbers.empty?
+
+    raise "Negative numbers not allowed #{negative_numbers.join(',')}"
+  end
 end
-  
